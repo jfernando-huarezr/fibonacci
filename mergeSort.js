c2997627a3ebd4ff9ff9c126e0423b9ac07a33d3
Jose Fernando Huarez Reyes <jose.huarezr@gmail.com>
2024-02-13 17:44:41 -0500

    merge sort completed

--- mergeSort.js
+++ mergeSort.js
@@ -1,1 +1,47 @@
-function mergeSort(arr) {}
+function mergeSort(array) {
+  // base case
+  if (array.length <= 1) return array;
+
+  // recursive definition
+  let mid = Math.floor(array.length / 2);
+  let left = [];
+  let right = [];
+
+  for (let i = 0; i < array.length; i++) {
+    if (i < mid) {
+      left.push(array[i]);
+    } else {
+      right.push(array[i]);
+    }
+  }
+
+  return merge(mergeSort(left), mergeSort(right));
+}
+
+function merge(left, right) {
+  let result = [];
+  let i = 0;
+  let j = 0;
+  while (i < left.length && j < right.length) {
+    if (left[i] < right[j]) {
+      result.push(left[i]);
+      i++;
+    } else {
+      result.push(right[j]);
+      j++;
+    }
+  }
+  result = [...result, ...left.slice(i), ...right.slice(j)];
+  return result;
+}
+
+let array = [];
+for (let i = 0; i < 30; i++) {
+  array.push(Math.floor(Math.random() * 1000) + 1);
+}
+
+console.time("mergesort");
+const sortedArray = mergeSort(array);
+console.timeEnd("mergesort");
+
+console.log(sortedArray);
